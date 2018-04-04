82f139cfda2bb1d200420ba339f3f2793d4667a1
Alex Hinds <alex.hinds141@gmail.com>
2018-04-04 19:31:44 +1000

    passing all GET

--- assignment2/db/userSchema.sql
+++ assignment2/db/userSchema.sql
@@ -19,4 +19,4 @@ INSERT INTO users (uid, username, passhash) VALUES (2, 'carol', '0xbeefcafebabe'
 
 INSERT INTO creds (uid, name, address, email, phonenum, funds) VALUES ('0', 'Alice Administrator', 'Omnipotent', 'alice@alice.com', '+313 373 8483', 31333337);
 INSERT INTO creds (uid, name, address, email, phonenum, funds) VALUES ('1', 'Bob Bandit', 'Nowhere', 'bob@bob.com', '-123 456 7890', 1337);
-INSERT INTO creds (uid, name, address, email, phonenum, funds) VALUES ('2', 'Sweet Carolina', 'Californ-eye-ay', 'carol@candle.lite', '+66 666 6666', 42);
+INSERT INTO creds (uid, name, address, email, phonenum, funds) VALUES ('2', 'Sweet Carolina', 'Californ-eye-ay', 'carol@candle.lite', '+66 666 6666', 41424344);
